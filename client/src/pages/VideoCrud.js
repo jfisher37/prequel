1556211bc22bc528ae4d3ea3862ffb93678c1012
aorich <rich.aidan@gmail.com>
2021-11-18 11:35:29 -0500

    merge

---
+++ client/src/pages/VideoCrud.js
@@ -0,0 +1,7 @@
+// import React from 'react';
+// import { useQuery } from '@apollo/client';
+
+// // import Video from '../components/VideoList';
+// import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react';
+
+// import { QUERY_VIDEOS } from '../utils/queries';
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,9 +3,11 @@ import { gql } from "@apollo/client";
 export const ADD_USER = gql`
   mutation addUser($name: String!, $email: String!, $password: String!) {
     addUser(name: $name, email: $email, password: $password) {
+      token {
         _id
         name
         email
+      }
     }
   }
 `;
