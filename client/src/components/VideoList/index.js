b529d61d91cb79e3dd764755f148d57147d943b7
lamperouge <philbohn1791@gmail.com>
2021-11-19 14:47:58 -0500

    added stylings back into individual video pages

--- client/src/components/VideoList/index.js
+++ client/src/components/VideoList/index.js
@@ -28,10 +28,14 @@ const VideoList = ({ videos }) => {
       {videos &&
         videos.map((video) => (
           <Card className="text-center my-3" key={video._id}>
-            <Card.Header as="h2">{video.title}</Card.Header>
-            <Card.Body>
-              <Card.Title>{video.publishDate}</Card.Title>
-              <div>Posted by: {video.videoAuthor}</div>
+            <Card.Header as="h2" className="video-title">
+              {video.title}
+            </Card.Header>
+            <Card.Body className="video-body">
+              <Card.Title className="roboto-font">
+                {video.publishDate}
+              </Card.Title>
+              <div className="roboto-font">Posted by: {video.videoAuthor}</div>
               <Link to={`/videos/${video._id}`} onClick={makeIncTrue}>
                 <video style={{ width: 660, height: "auto" }}>
                   <source src={video.cloudURL} type="video/mp4" />
