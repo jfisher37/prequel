6df2d73cea7ed6300c60246e9daceeb2e9fcbf65
lamperouge <philbohn1791@gmail.com>
2021-11-19 10:28:09 -0500

    started basic stylings in cards for the Video.js and VideoCrud.js pages

--- client/src/pages/Video.js
+++ client/src/pages/Video.js
@@ -1,33 +1,37 @@
-import React from 'react';
+import React from "react";
 
 // Import the `useParams()` hook from React Router
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
 
-import { QUERY_SINGLE_VIDEO } from '../utils/queries';
+import { QUERY_SINGLE_VIDEO } from "../utils/queries";
+
+import Card from "react-bootstrap/Card";
 
 const SingleVideo = () => {
-    const { videoId } = useParams();
+  const { videoId } = useParams();
 
-    const { loading, data } = useQuery(QUERY_SINGLE_VIDEO, {
-        variables: { videoId: videoId },
-    });
+  const { loading, data } = useQuery(QUERY_SINGLE_VIDEO, {
+    variables: { videoId: videoId },
+  });
 
-    const video = data?.video || {};
+  const video = data?.video || {};
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-    return (
-        <div>
-            <div>{video.title}</div>
-            <div>{video.publishDate}</div>
-            <video style={{ width: 660, height: 'auto' }} controls>
-                <source src={video.cloudURL} type="video/mp4" />
-            </video>
-        </div>
-    )
+  return (
+    <Card className="text-center">
+      <Card.Header as="h2">{video.title}</Card.Header>
+      <Card.Body>
+        <Card.Title>{video.publishDate}</Card.Title>
+        <video style={{ width: 660, height: "auto" }} controls>
+          <source src={video.cloudURL} type="video/mp4" />
+        </video>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default SingleVideo;
