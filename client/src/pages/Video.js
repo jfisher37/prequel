85f0d39852fe494c02c7030a9300ecb060bb9627
Nicole Pingar <nicolepingar@yahoo.com>
2021-11-20 12:21:43 -0500

    cache video metrics

--- client/src/pages/Video.js
+++ client/src/pages/Video.js
@@ -10,59 +10,45 @@ import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 
 const SingleVideo = () => {
-  const { videoId } = useParams();
-  const [videoMetrics, { error }] = useMutation(VIDEO_METRICS);
-  const { loading, data } = useQuery(QUERY_SINGLE_VIDEO, {
-    variables: { videoId: videoId },
-  });
-  console.log(data);
-  const [like, setLike] = useState(data?.video.likes);
-  const [dislike, setDisLike] = useState(data?.video.dislikes);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  } else {
-    const video = data?.video || {};
 
-    const updateMetrics = async () => {
-      const newView = video.views + 1;
-
+  const [videoMetrics, { error }] = useMutation(VIDEO_METRICS, {
+    update(cache, { data: { videoMetrics } }) {
       try {
-        await videoMetrics({
-          variables: {
-            videoId: videoId,
-            likes: like,
-            dislikes: dislike,
-            views: newView,
-          },
-        });
-      } catch (err) {
-        console.log(err);
+        const { banana } = cache.readQuery({ query: QUERY_SINGLE_VIDEO });
+        console.log(banana);
+        cache.writeQuery({
+          query: QUERY_SINGLE_VIDEO,
+          data: { banana: [...banana, videoMetrics] }
+        })
+
+      } catch (e) {
+        console.error(e);
       }
-    };
+    }
+  });
 
-    updateMetrics();
 
-    return (
-      <div>
-        <Container>
-          <Card className="text-center my-3">
-            <Card.Header as="h2">{video.title}</Card.Header>
-            <Card.Body>
-              <Card.Title>{video.publishDate}</Card.Title>
-              <Card.Title>Views: {video.views}</Card.Title>
-              <video style={{ width: 660, height: "auto" }} controls>
-                <source src={video.cloudURL} type="video/mp4" />
-              </video>
-              <p>Likes: {video.likes} Dislikes: {video.dislikes}</p>
-              <p><button onClick={() => { updateMetrics(setLike(data?.video.likes + 1)) }}><i class="fas fa-thumbs-up"></i></button>
-                <button onClick={() => { updateMetrics(setDisLike(data?.video.dislikes + 1)) }}><i class="fas fa-thumbs-down"></i></button></p>
-            </Card.Body>
-          </Card>
-        </Container>
-      </div>
-    );
-  }
-};
+  return (
+    <div>
+      <Container>
+        <Card className="text-center my-3">
+          <Card.Header as="h2">{video.title}</Card.Header>
+          <Card.Body>
+            <Card.Title>{video.publishDate}</Card.Title>
+            <Card.Title>Views: {video.views}</Card.Title>
+            <video style={{ width: 660, height: "auto" }} controls>
+              <source src={video.cloudURL} type="video/mp4" />
+            </video>
+            <p>Likes: {video.likes} Dislikes: {video.dislikes}</p>
+            <p><button onClick={updateMetrics}><i class="fas fa-thumbs-up"></i></button>
+              <button onClick={updateMetrics}><i class="fas fa-thumbs-down"></i></button></p>
+          </Card.Body>
+        </Card>
+      </Container>
+    </div>
+  );
+}
+
 
 export default SingleVideo;
